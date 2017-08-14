1d3fdbb2cb5aeaeef4c9ca0a6ed2f06e53fa5942
dagou <green711@126.com>
2017-08-14 22:01:49 +0800

    change ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -30,20 +30,20 @@ module.exports = {
    */
   deploy : {
     production : {
-      user : 'node',
-      host : '212.83.163.1',
+      user : 'root',
+      host : '39.108.67.244',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
+      repo : 'git@github.com:abbbbXd/dogeblog.git',
       path : '/var/www/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
     },
     dev : {
-      user : 'node',
-      host : '212.83.163.1',
+      user : 'root',
+      host : '39.108.67.244',
       ref  : 'origin/master',
-      repo : 'git@github.com:repo.git',
+      repo : 'git@github.com:abbbbXd/dogeblog.git',
       path : '/var/www/development',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env dev',
       env  : {
         NODE_ENV: 'dev'
       }
